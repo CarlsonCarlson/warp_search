4585f4910ea4e8c3fbce1eedcad5d3aee7709031
CarlsonCarlson <carlson.cheng888@gmail.com>
2020-08-30 13:46:33 -0400

    reinitializing main branch using a sample starting base code from https://www.tomforth.co.uk/chromeextension/

--- background.js
+++
@@ -1,6 +0,0 @@
-// Background Scripts: Persistent ones stay and wait for the user to interact with the
-// extension.
-// Event pages (non-persistent): only loaded in response to some event
-console.log("Warp Drive Ready!!!");
-
-// This is probably where I'm going to be putting my python backend in some way or another
